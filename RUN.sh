5a2d8008823354e1b78266fde36a051229eebd96
NJAU-CDSIC <142595806+NJAU-CDSIC@users.noreply.github.com>
2024-07-15 22:04:53 +0800

    Add files via upload

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,8 @@
-nohup Rscript --vanilla initData.R /Volumes/zz_Seagate/CDSIC/R/UGithub/params.json  2>&1 | tee initData.out || { echo 'initData.R failed' ; exit 1; } &
+## Analyze data of type mode1
+nohup Rscript --vanilla initData.R /Volumes/zz_Seagate/CDSIC/R/UGithub/params_1.json  2>&1 | tee initData.out || { echo 'initData_1.R failed' ; exit 1; } &
 
+## Analyze data of type mode2
+nohup Rscript --vanilla initData.R /Volumes/zz_Seagate/CDSIC/R/UGithub/params_2.json  2>&1 | tee initData_2.out || { echo 'initData_2.R failed' ; exit 1; } &
 
+## Analyze data of type mode3
+nohup Rscript --vanilla initData.R params_3.json  2>&1 | tee initData_3.out || { echo 'initData_3.R failed' ; exit 1; } &
